4f3ffe632b47549bb45f9a94afc1a7b31c91a56e
Piyush Wagh <piyushswagh2005@gmail.com>
2026-04-02 16:37:33 +0530

    Added backend project

---
+++ Backend_projects/Project_camp_backend/src/validators/index.js
@@ -0,0 +1,111 @@
+import {body} from "express-validator"
+import { AvailableUserRole } from "../utils/constants"
+
+
+const userRegisterValidator = () =>{
+
+    return [
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid"),
+        body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLowercase()
+        .withMessage("Username must be in lower case")
+        .isLength({min:3})
+        .withMessage("Username must be at least 3 characters long"),
+        body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required"),
+        body("fullName")
+        .trim()
+        
+    ]
+
+}
+
+const userLoginValidator = () =>{
+    return [
+        body("email").optional()
+        .isEmail().withMessage("Email is invalid"),
+        body("password").notEmpty()
+        .withMessage("Password is required")
+    ]
+}
+
+
+const userChangeCurrentPasswordValidator = () =>{
+    return [
+        body("oldPassword").notEmpty()
+        .withMessage("Old password is required"),
+        body("newPassword").notEmpty().withMessage("New password is required"),
+    ]
+}
+
+
+const userForgotPassowordValidator = () =>{
+    return [
+        body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid"),
+    ]
+}
+
+
+const userResetForgotPasswordValidator = () =>{
+
+    return [
+        body("newPassword")
+        .notEmpty()
+        .withMessage("Password is required")
+    ]
+}
+
+
+
+const createProjectValidator = () =>{
+    return[
+        body("name")
+        .notEmpty()
+        .withMessage("Name is required"),
+        body("description")
+        .optional(),
+    ]
+}
+
+
+const addMembertoProjectValidator = () =>{
+    return [
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid"),
+        body("role")
+        .notEmpty()
+        .withMessage("Role is required")
+        .isIn(AvailableUserRole)
+        .withMessage("Role is invalid")
+    ]
+}
+
+export {
+    userRegisterValidator,
+    userLoginValidator,
+    userChangeCurrentPasswordValidator,
+    userForgotPassowordValidator,
+userResetForgotPasswordValidator,
+createProjectValidator,
+addMembertoProjectValidator
+    
+
+}
